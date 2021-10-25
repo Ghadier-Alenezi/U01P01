9b260f38164649e5476393462562015ae12ca788
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-10-26 00:10:09 +0300

    project initial setup

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ let mainArray = [
     <p>A classic car is an older car, typically older than 20 years,
     The common theme is of an older car of sufficient historical interest to be
     collectible and worth preserving or restoring rather than scrapping.
-    Classic cars are a subset of a broader category of "collector cars"
-    (which includes both restored classic cars vehicles and newer exotic vehicles).
-    A subset of what is considered classic cars are known as antique cars
-    (manufactured before 1980) or vintage cars (manufactured pre-World War II.</p>`,
+    Classic cars are a subset of a broader category of "collector cars"</p>`,
     imgM: "./project1cars/classic-cars/classicM.jpg",
+    extraImg: [
+      "./project1cars/classic-cars/classic2.jpg",
+      "./project1cars/classic-cars/classic3.jpg",
+      "./project1cars/classic-cars/classic4.jpg",
+    ],
   },
   {
     category: "Sports Cars",
@@ -20,10 +22,13 @@ let mainArray = [
     <p>A sports car is a car designed with an emphasis on dynamic performance,
     such as handling, acceleration, top speed, or thrill of driving. Sports cars
     originated in Europe in the early 1900s and are currently produced by many
-    manufacturers around the world.
-    Definitions of sports cars often relate to how the car design is optimised for
-    dynamic performance,[2][3] without any specific minimum requirements.</p>`,
+    manufacturers around the world.</p>`,
     imgM: "project1cars/sport-cars/sportsM.jpg",
+    extraImg: [
+      "./project1cars/sport-cars/sports3.jpg",
+      "./project1cars/sport-cars/sports4.jpg",
+      "./project1cars/sport-cars/sports2.jpg",
+    ],
   },
   {
     category: "Electric Cars",
@@ -34,24 +39,53 @@ let mainArray = [
     internal combustion engine (ICE) vehicles, electric cars are quieter, have no
     exhaust emissions, and lower emissions overall.</p>`,
     imgM: "./project1cars/electric-cars/electricM.jpg",
+    extraImg: [
+      "./project1cars/electric-cars/electric3.jpg",
+      "./project1cars/electric-cars/electric2.jpg",
+      "./project1cars/electric-cars/electric4.jpg",
+
+    ],
   },
 ];
 
-const openCard = () => {
-  $(".hidePage").hide();
-
-};
-
 // render function
 const render = () => {
-  //   $("cards").html("");
   mainArray.forEach((item, i) => {
-    $("#cards").append(`<li class="card">
+    $("#cards").append(`<div class="card">
     <img id= "cardImg-${i}" src="${item.imgCar}" width="100%" />
     <p id= "cardCtg-${i}">${item.category}</p>
-    <button id= "cardBtn-${i}">More Info</button></li>`);//end of append
-    $("#cardBtn-"+i).click(()=> {openCard(i)});
+    <button onclick='renderOneItem(${i})' id= "cardBtn-${i}">More Info</button></div>`);
   });
 };
-
 render();
+
+// render item function
+function renderOneItem(i) {
+  $(".hidePage").hide();
+  const myItem = mainArray[i];
+  $(".descCat").append(`<div>
+  <p class="cardDesc">${myItem.describe}</p>
+   <img class="imgM-${i}" src="${myItem.imgM}" />
+     <div id='extraImges'></div>
+     </div>`);
+  myItem.extraImg.forEach((item, i) => {
+    $("#extraImges").append(`<div>
+       <img  src='${item}' /><div>`);
+  });
+}
+
+// // about us page
+// function renderAbouUs(){
+//   $(".hidePage").hide();
+//   $(".aboutUs").append(`<div>
+//   <h1>WELCOME</h1>
+//   <p>I'm pleased to have you here. My name is Ghadier a lucky girl to have this opportunity to work with such a community of programers. I look forward to collaborate with you in our bright future. KEEP UP THE GOOD WORK!</p>
+//   </div>`)
+// };
+// //search more about how to local storge JSON
+// // logo + fav + search + redesig the whole page
+
+
+
+
+//save fav
